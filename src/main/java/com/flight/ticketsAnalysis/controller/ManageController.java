e07170363e3ca061aaee93c92f31fd13e3a13193
4ilde <wilde08@163.com>
2019-08-17 15:22:25 +0800

    用户管理

--- src/main/java/com/flight/ticketsAnalysis/controller/ManageController.java
+++ src/main/java/com/flight/ticketsAnalysis/controller/ManageController.java
@@ -1,4 +1,22 @@
 package com.flight.ticketsAnalysis.controller;
 
+import com.flight.ticketsAnalysis.entity.UserEntity;
+import com.flight.ticketsAnalysis.service.ManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 public class ManageController {
+    @Autowired
+    private ManageService manageService;
+
+    @RequestMapping("queryUser")
+    @ResponseBody
+    public Object queryUser(){
+        List<UserEntity> list = manageService.queryUser();
+        return list;
+    }
 }
