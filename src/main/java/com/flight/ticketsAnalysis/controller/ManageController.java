a445561ffee20b85123711f44482d8f019edc58c
SSshuishui <1580746585@qq.com>
2019-08-19 16:25:03 +0800

    第八天：权限控制

--- src/main/java/com/flight/ticketsAnalysis/controller/ManageController.java
+++ src/main/java/com/flight/ticketsAnalysis/controller/ManageController.java
@@ -1,4 +1,15 @@
 package com.flight.ticketsAnalysis.controller;
 
+
+import com.flight.ticketsAnalysis.service.ManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ManageController")
 public class ManageController {
+
+    @Autowired
+    private ManageService manageService;
 }
--- src/main/java/com/flight/ticketsAnalysis/dao/LoginMapper.java
+++ src/main/java/com/flight/ticketsAnalysis/dao/LoginMapper.java
@@ -3,6 +3,8 @@ package com.flight.ticketsAnalysis.dao;
 
 import com.flight.ticketsAnalysis.entity.AdminEntity;
 import com.flight.ticketsAnalysis.entity.UserEntity;
+import org.apache.catalina.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -14,8 +16,15 @@ public interface LoginMapper {
 
     //登录模块
     @Select("select * from admin where adminname = #{username} and password = #{password}")
-    public List<AdminEntity> selectAdminAccount(@Param("username") String username, @Param("password") String password);
+    public AdminEntity selectAdminAccount(@Param("username") String username, @Param("password") String password);
 
     @Select("select * from user where username = #{username} and password = #{password}")
-    public List<UserEntity> selectUserAccount(@Param("username") String username, @Param("password") String password);
+    public UserEntity selectUserAccount(@Param("username") String username, @Param("password") String password);
+
+    //注册模块
+    @Insert("INSERT INTO user ( email, username, password) VALUES (#{email},#{username},#{password})")
+    public boolean insertUser(@Param("email") String email, @Param("username") String username, @Param("password") String password);
+
+    @Select("select * from user where username = #{username}")
+    public List<UserEntity> selectUser(@Param("username") String username);
 }
